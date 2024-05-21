1e273ba842a0c2d76a09f1cdfbd5e3985dcac582
Nurbekprog <nurprogdata@gmail.com>
2024-05-21 09:33:00 +0500

    exam crypto coding

---
+++ src/context/DataContext.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { createContext } from "react";
+
+export const DataContext = createContext();
+
+export const DataProvider = ({ children}) => {
+    const [type, setType] = useState(localStorage.getItem("type") || "USD");
+    const [watch, setWatch] = useState(
+        JSON.parse(localStorage.getItem("watchList")) || []
+    );
+
+    return (
+        <DataContext.Provider value={[type, setType, watch, setWatch]}>
+            {children}
+        </DataContext.Provider>
+    )
+}
